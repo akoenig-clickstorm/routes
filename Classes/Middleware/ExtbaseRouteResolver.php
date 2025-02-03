27259f93b6fb1808ad59c3b9e3c7923f90ec7072
Alexander König <koenig@clickstorm.de>
2025-02-03 13:54:02 +0100

    [TASK] remove unsetting of routing attribute in middleware https://github.com/Lacr1ma/routes/pull/20/files

--- Classes/Middleware/ExtbaseRouteResolver.php
+++ Classes/Middleware/ExtbaseRouteResolver.php
@@ -64,7 +64,6 @@ class ExtbaseRouteResolver implements MiddlewareInterface
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
         $request = $request->withAttribute('extbase', new ExtbaseRequestParameters());
-        $request = $this->disableRouting($request);
 
         try {
             $this->apiHandler->handle($request);
@@ -86,17 +85,5 @@ class ExtbaseRouteResolver implements MiddlewareInterface
         }
 
         return $this->apiHandler->generateResponse();
-    }
-
-    /**
-     * We unset routing attribute to prevent it from next middleware redirects/resolves
-     */
-    private function disableRouting(ServerRequestInterface $request): ServerRequestInterface
-    {
-        $request = $request->withoutAttribute('routing');
-
-        $GLOBALS['TYPO3_REQUEST'] = $request;
-
-        return $request;
     }
 }
